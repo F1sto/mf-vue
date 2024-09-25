aa579df8853b03a9ff31c8d291761d8bdea6320b
andrei misyurin <andrew.misiyrin@gmail.com>
2024-09-26 01:43:31 +0600

    integration with root-app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <h2 class="vue-cli__title">Vue cli App</h2>
+
+    <nav>
+      <router-link to="/vue-cli/">Home</router-link> |
+      <router-link to="/vue-cli/about">About</router-link>
+    </nav>
+  </div>
   <router-view />
 </template>
 
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const rootElement = document.getElementById(
+    "single-spa-application:@micro/vue-cli"
+  );
+  rootElement.classList.add("vue-cli");
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: #1d2a39;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #195365;
     }
+  }
+}
+
+.vue-cli {
+  background: #42b983;
+  border: 2px solid #278359;
+
+  &__title {
+    font-size: 50px;
+    color: #424d57;
   }
 }
 </style>
